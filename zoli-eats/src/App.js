16dcab822a2ca4bbf56c1375195906eed18a59f2
Preda Alexandru <alexpreda95@yahoo.com>
2023-01-20 16:33:52 +0200

    Creating Hero and Cards sections

--- zoli-eats/src/App.js
+++ zoli-eats/src/App.js
@@ -1,9 +1,13 @@
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCards />
     </div>
   );
 }
