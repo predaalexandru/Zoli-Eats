64526d0d9f8a08bdc5539fb0cf95e8b65d6ad38d
Preda Alexandru <alexpreda95@yahoo.com>
2023-01-25 15:24:31 +0200

    Created the food section

---
+++ zoli-eats/src/data/data.js
@@ -0,0 +1,130 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Double Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.pexels.com/photos/1556698/pexels-photo-1556698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$$',
+    },
+    {
+      id: 2,
+      name: 'Bacon Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.pexels.com/photos/2267538/pexels-photo-2267538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$',
+    },
+    {
+      id: 3,
+      name: 'Pulled Pork Burger',
+      category: 'burger',
+      image:
+        'https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 4,
+      name: 'Big Tasty',
+      category: 'burger',
+      image:
+        'https://images.pexels.com/photos/103886/pexels-photo-103886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 5,
+      name: 'Sea food pasta',
+      category: 'pasta',
+      image:
+        'https://images.pexels.com/photos/2092906/pexels-photo-2092906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 6,
+      name: 'Bolognese',
+      category: 'pasta',
+      image:
+        'https://images.pexels.com/photos/116738/pexels-photo-116738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 7,
+      name: 'Margherita Pizza',
+      category: 'pizza',
+      image:
+        'https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$$',
+    },
+    {
+      id: 8,
+      name: 'Protein Pizza',
+      category: 'pizza',
+      image:
+        'https://images.pexels.com/photos/845812/pexels-photo-845812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$',
+    },
+    {
+      id: 9,
+      name: 'Salami Pizza',
+      category: 'pizza',
+      image:
+        'https://images.pexels.com/photos/3682837/pexels-photo-3682837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 10,
+      name: 'Vegetable Pizza',
+      category: 'pizza',
+      image:
+        'https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+    {
+      id: 11,
+      name: 'Chicken Pasta',
+      category: 'pasta',
+      image:
+        'https://images.pexels.com/photos/3214161/pexels-photo-3214161.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$$',
+    },
+    {
+      id: 12,
+      name: 'Mac and Cheese',
+      category: 'pasta',
+      image:
+        'https://images.pexels.com/photos/154145/pexels-photo-154145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$',
+    },
+    {
+      id: 13,
+      name: 'Wings',
+      category: 'chicken',
+      image:
+        'https://images.pexels.com/photos/1123249/pexels-photo-1123249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$',
+    },
+    {
+      id: 14,
+      name: 'Baked Chicken',
+      category: 'chicken',
+      image:
+        'https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$$',
+    },
+    {
+      id: 15,
+      name: 'Chicken Tenders',
+      category: 'chicken',
+      image:
+        'https://images.pexels.com/photos/1059943/pexels-photo-1059943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$',
+    },
+    {
+      id: 16,
+      name: 'Chicken Kebab',
+      category: 'chicken',
+      image:
+        'https://images.pexels.com/photos/60616/fried-chicken-chicken-fried-crunchy-60616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: '$$$',
+    },
+  ];
